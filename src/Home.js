32e5a3bcb051aa183cf2918cf75fb68c6304fffc
Taylor <taylorpollioverturf@gmail.com>
2022-01-08 20:57:48 -0800

    homepage and photos page use API call data successfully

--- src/Home.js
+++ src/Home.js
@@ -5,49 +5,42 @@ const KEY = process.env.REACT_APP_NASA_API_KEY;
 
 function Home() {
 	// Use State
-	const [imageURL, setImageURL] = useState();
-	const [fullCameraName, setFullCameraName] = useState();
-	const [shortCameraName, setShortCameraName] = useState();
-	const [date, setDate] = useState();
-	const [roverName, setRoverName] = useState();
-
-	// Random Number Generator for Photos
-	const randomPhoto = Math.floor(Math.random() * 855);
+	const [imageOfTheDay, setImageOfTheDay] = useState();
+	const [imageTitle, setImageTitle] = useState();
+	const [imageInfo, setImageInfo] = useState();
+	const [authorCredit, setAuthorCredit] = useState(); 
 
 	// API Call
-	const makeAPICall = () =>
-		fetch(
-			`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${KEY}`
-		)
+	const makeAPICall2 = () =>
+		fetch(`https://api.nasa.gov/planetary/apod?api_key=${KEY}`)
 			.then((response) => response.json())
 			.then((results) => {
-				// console.log(results);
-				setFullCameraName(results.photos[randomPhoto].camera.full_name);
-				setShortCameraName(results.photos[randomPhoto].camera.name);
-				setImageURL(results.photos[randomPhoto].img_src);
-				setDate(results.photos[randomPhoto].earth_date);
-				setRoverName(results.photos[randomPhoto].rover.name);
+				// console.log(results.url);
+				setImageOfTheDay(results.url);
+				setImageTitle(results.title);
+				setImageInfo(results.explanation);
+				setAuthorCredit(results.copyright);
 			});
 
 	// Date Conversion
 
 	// Use Effect
 	useEffect(() => {
-		makeAPICall();
-		// convert();
+		makeAPICall2();
 	}, []);
 
 	return (
-		<>
-			<h1>Spacestagram 🪐</h1>
-			<div className='homepage-photo'>
-				<h3>{shortCameraName} Footage</h3>
-				<img id='testing' src={imageURL} alt='MARS rover footage' />
-				<p>
-					{roverName} at {fullCameraName}, {date}
-				</p>
-			</div>
-		</>
+		<div>
+			<h1>Welcome to Spacestagram 🪐</h1>
+			<img
+				id='homepage-wrapper'
+				src={imageOfTheDay}
+				alt='NASA footage of the day'
+			/>
+			<h3>{imageTitle}</h3>
+			<h4>{authorCredit}</h4>
+			<p>{imageInfo}</p>
+		</div>
 	);
 }
 
--- src/Photos.js
+++ src/Photos.js
@@ -1,13 +1,53 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 // import './Home.css';
 // import Footer from '../Footer/Footer';
 // import Menu from '../NavBar/NavBar';
 
+const KEY = process.env.REACT_APP_NASA_API_KEY;
+
 function Photos() {
+	
+	// Use State
+	const [imageURL, setImageURL] = useState();
+	const [fullCameraName, setFullCameraName] = useState();
+	const [shortCameraName, setShortCameraName] = useState();
+	const [date, setDate] = useState();
+	const [roverName, setRoverName] = useState();
+
+	// Random Number Generator for Photos
+	const randomPhoto = Math.floor(Math.random() * 855);
+
+	// API Call
+	const makeAPICall = () =>
+		fetch(
+			`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${KEY}`
+		)
+			.then((response) => response.json())
+			.then((results) => {
+				// console.log(results);
+				setFullCameraName(results.photos[randomPhoto].camera.full_name);
+				setShortCameraName(results.photos[randomPhoto].camera.name);
+				setImageURL(results.photos[randomPhoto].img_src);
+				setDate(results.photos[randomPhoto].earth_date);
+				setRoverName(results.photos[randomPhoto].rover.name);
+			});
+
+	// Use Effect
+	useEffect(() => {
+		makeAPICall();
+	}, []);
+
 	return (
 		<>
 			<h1>Here are some photos of space.</h1>
+			<div className='homepage-photo'>
+				<h3>{shortCameraName} Footage</h3>
+				<img id='testing' src={imageURL} alt='MARS rover footage' />
+				<p>
+					{roverName} at {fullCameraName}, {date}
+				</p>
+			</div>
 		</>
 	);
 }
